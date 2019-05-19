a162a2e33b83f0dff5dc40df2d3143967eabab6c
Annie Truex <artruex@gmail.com>
2019-05-19 22:29:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma (greeting)
   if greeting == 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif greeting != greeting.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts "NO, NOT SINCE 1938!""HUH?! SPEAK UP, SONNY!"
   else
-    return "NO, NOT SINCE 1938!"
+    return 
   end
 end
